e65c490a927f713e4715f0783cd80c4bbdf80d30
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2024-10-21 16:50:13 +0200

    add try catch

--- src/controllers/tools.controller.ts
+++ src/controllers/tools.controller.ts
@@ -1,7 +1,7 @@
 // External dependencies
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import pkg from 'symbology'
-const { SymbologyType, createStream } = pkg
+const { SymbologyType, createStream, OutputType, EncodingMode } = pkg
 
 declare module 'fastify' {
   export interface FastifyReply {
@@ -12,7 +12,7 @@ declare module 'fastify' {
 }
 
 export default async function (fastify: FastifyInstance) {
-  fastify.get('/tools/barcode/:format/:value', async function (request: FastifyRequest<{
+  fastify.get('/barcode/:format/:value', async function (request: FastifyRequest<{
     Params: {
       format: string,
       value: string
@@ -20,15 +20,24 @@ export default async function (fastify: FastifyInstance) {
   }>, reply: FastifyReply) {
     const start = performance.now()
 
-    const { data } = await createStream({
-      symbology: SymbologyType.CODE128B
-    }, request.params.value)
+    try {
+      const { data } = await createStream(
+        {
+          symbology: SymbologyType.CODE128B,
+          encoding: EncodingMode.GS1_MODE
+        },
+        request.params.value,
+        OutputType.PNG
+      )
 
-
-    reply.header('Cache-Control', 'must-revalidate, max-age=172800, private')
-      .header('Expires', new Date(new Date().getTime() + 172800000).toUTCString())
-      .header('Last-Modified', new Date().toUTCString())
-      .type('image/png')
-      .send(data)
+      reply.header('Cache-Control', 'must-revalidate, max-age=172800, private')
+        .header('Expires', new Date(new Date().getTime() + 172800000).toUTCString())
+        .header('Last-Modified', new Date().toUTCString())
+        .type('image/png')
+        .send(data)
+    } catch (err) {
+      request.log.error({ error: err }, 'error while generating png')
+      return reply.error('error while generating png!', 500, performance.now() - start)
+    }
   })
 }
