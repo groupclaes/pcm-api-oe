ec218a8fc917f0c3726024ee9a0bae52bdf65033
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2024-10-21 16:53:06 +0200

    fix loglevel

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,20 @@ import fileController from './controllers/file.controller'
 import toolsController from './controllers/tools.controller'
 import documentlistController from './controllers/documentlist.controller'
 import objectlistController from './controllers/objectlist.controller'
+
 // http://pcm.groupclaes.be/v4/oe/tools/barcode/code128/734242
 
-const LOGLEVEL = 'warn'
+const LOGLEVEL = 'debug'
 
 /** Main loop */
 export default async function (config: any): Promise<FastifyInstance | undefined> {
   if (!config.wrapper.mssql && config.mssql) config.wrapper.mssql = config.mssql
   if (!config.wrapper.fastify.requestLogging) config.wrapper.fastify.requestLogging = true
+
   const fastify = await Fastify({ ...config.wrapper, securityHeaders: { csp: `default-src 'self' 'unsafe-inline' pcm.groupclaes.be` } })
+  fastify.log.level = LOGLEVEL
   const version_prefix = (env.APP_VERSION ? '/' + env.APP_VERSION : '')
+
   await fastify.register(fileController, { prefix: `${version_prefix}/${config.wrapper.serviceName}/file`, logLevel: LOGLEVEL })
   await fastify.register(toolsController, { prefix: `${version_prefix}/${config.wrapper.serviceName}/tools`, logLevel: LOGLEVEL })
   await fastify.register(documentlistController, { prefix: `${version_prefix}/${config.wrapper.serviceName}/documentlist`, logLevel: LOGLEVEL })
